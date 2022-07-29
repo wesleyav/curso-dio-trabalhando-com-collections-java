898e115c270f796945486ebbd4ed96489ce81d56
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2022-07-29 09:30:43 -0300

    Conhecendo os métodos List - parte 1

---
+++ src/exer1/Main.java
@@ -0,0 +1,47 @@
+package exer1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Crie uma lista e adiciona as sete notas: ");
+		List<Double> notas = new ArrayList<Double>();
+		notas.add(7.0);
+		notas.add(8.5);
+		notas.add(9.3);
+		notas.add(5.0);
+		notas.add(7.0);
+		notas.add(0.0);
+		notas.add(3.0);
+		System.out.println(notas.toString());
+		
+		System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d));
+		
+		System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+		notas.add(4, 8d);
+		System.out.println(notas);
+		
+		System.out.println("Substitue a nota 5.0 pela nota 6.0: ");
+		notas.set(notas.indexOf(5d), 6.0);
+		System.out.println(notas);
+		
+		System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
+		
+		System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
+		for(Double nota : notas) {
+			System.out.println(nota);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
